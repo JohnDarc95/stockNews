904dca11037659f5b2859315746bab7004265d05
John D'Arcangelo <johndarc95@gmail.com>
2018-03-11 10:56:32 -0400

    track C#

---
+++ C#/hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class HelloWorld
+{
+    static public void Main()
+    {
+        Console.WriteLine("Hello Mono World");
+
+        string x;
+
+        x = Console.ReadLine();
+
+        Console.WriteLine(x);
+    }
+}
